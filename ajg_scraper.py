ed3543650b0597e2ff3252ce5e5a72253837218a
raquelalvesm <pi100348@pi.trf1.gov.br>
2026-09-21 13:24:55 -0300

    fix: radio INSS/nao-INSS apenas via XPATH (removido click por ID que revertia)

--- ajg_scraper.py
+++ ajg_scraper.py
@@ -187,11 +187,12 @@ class ScraperAJG:
         if 'INSS' in dados.get('polo_passivo', '').upper():
             self.driver.find_element(By.XPATH,
                 '/html/body/form/div[3]/span/table/tbody/tr[7]/td[2]/fieldset/span/span/input[1]').click()
+            self.log('  Radio: SIM (INSS)')
         else:
             self.driver.find_element(By.XPATH,
                 '/html/body/form/div[3]/span/table/tbody/tr[7]/td[2]/fieldset/span/span/input[2]').click()
+            self.log('  Radio: NAO (nao-INSS)')
 
-        self.driver.find_element(By.ID, 'formAJIntranet:concBenefAssistDeficBenefPrevIncLab').click()
         self.log('  Dados da nomeacao preenchidos.')
         self.driver.find_element(By.ID, 'formAJIntranet:avancar').click()
 
